64eb4d396e060f532171d498059130796bf1433f
Christopher Zell <zelldon91@googlemail.com>
2019-04-18 13:28:12 +0200

    chore(logstreams): revert renaming StateStorage

--- broker-core/src/test/java/io/zeebe/broker/transport/clientapi/ClientApiMessageHandlerTest.java
+++ broker-core/src/test/java/io/zeebe/broker/transport/clientapi/ClientApiMessageHandlerTest.java
@@ -37,7 +37,7 @@ import io.zeebe.logstreams.LogStreams;
 import io.zeebe.logstreams.log.BufferedLogStreamReader;
 import io.zeebe.logstreams.log.LogStream;
 import io.zeebe.logstreams.log.LoggedEvent;
-import io.zeebe.logstreams.state.DataStorage;
+import io.zeebe.logstreams.state.StateStorage;
 import io.zeebe.protocol.Protocol;
 import io.zeebe.protocol.clientapi.ErrorCode;
 import io.zeebe.protocol.clientapi.ErrorResponseDecoder;
@@ -119,7 +119,7 @@ public class ClientApiMessageHandlerTest {
       e.printStackTrace();
     }
 
-    final DataStorage stateStorage = new DataStorage(runtime, snapshots);
+    final StateStorage stateStorage = new StateStorage(runtime, snapshots);
 
     final String logName = "test";
 
